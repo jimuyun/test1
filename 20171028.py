a0438b6178d0a977bb066278f8a92b32c0572b19
jimuyun <1056678957@qq.com>
2017-11-05 17:37:04 +0800

    t1

---
+++ 20171028.py
@@ -0,0 +1,25 @@
+print 'jimuyun'
+
+print 12+15-7*3+10/2
+
+print 'hello',
+print 'world'
+
+print 'hello\nword'
+
+
+a = 'hello,world'
+b = 1.414
+c = (1 != 2)
+d = (True and False)
+e = (True or False)
+
+print ("a:%s;b:%.2f;c:%s;d:%s;e:%s;") % (a,b,c,d,e)
+
+m = "'hello,world'"
+n='\\"hello,world"'
+print m
+print n
+
+o="'hello,world'\n\\\"hello,world\""
+print o
